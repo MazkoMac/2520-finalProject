93a0709f8aa94ee38a3b217e83ff737cdf904aef
github_MazkoMac <mazkomac@gmail.com>
2018-05-10 14:08:54 -0700

    change port

--- server.js
+++ server.js
@@ -142,6 +142,6 @@ function checkRecords() {
     }
 }
 
-app.listen(process.env.PORT || 8002, () => {
+app.listen(process.env.PORT || 8003, () => {
     console.log('Server is up on the port 8000');
 });
